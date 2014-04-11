2a28ac033084a34711cb53d347dc6f4242f52526
Dan Johnson <dan@balancerockmedia.com>
2014-04-11 11:43:09 -0400

    edits

--- markets-ui/test/unit/domain/Market.js
+++ markets-ui/test/unit/domain/Market.js
@@ -1,12 +1,34 @@
 // mocha --bail --reporter spec
 
-define(['chai', 'app/domain/Market'], function(chai, Market) {
+define(['backbone', 'chai', 'app/domain/Market', 'backbone.validation'], function(Backbone, chai, Market) {
   var assert = chai.assert;
   
-  describe('Math', function() {
-    it('adds', function() {
-      assert.equal(1+ 1, 2);
+  beforeEach(function() {
+    _.extend(Backbone.Model.prototype, Backbone.Validation.mixin);
+  });
+  
+  describe('Market', function() {
+      
+    it('validation passes if required attributes are set', function() {
+      var market = new Market();
+      
+      market.set('name', 'test');
+      market.set('location_id', 1);
+      market.set('currency_id', 1);
+      
+      var isValid = market.isValid(true);
+        
+      assert.equal(isValid, true);
     });
+    
+    it('validation fails if required attributes are not set', function() {
+      var market = new Market();
+      
+      var isValid = market.isValid(true);
+        
+      assert.equal(isValid, false);
+    });
+    
   });
   
 });
