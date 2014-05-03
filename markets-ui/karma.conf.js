be412be2d70bca487f81c0ff29a3aee16be38b5f
Dan Johnson <dan@balancerockmedia.com>
2014-05-02 23:36:33 -0400

    edits

--- markets-ui/karma.conf.js
+++ markets-ui/karma.conf.js
@@ -72,6 +72,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: true,
+    
+    
+    coverageReporter: {
+      type : 'lcov'
+    }
   });
 };
