8db1225befd7b080aacf40164152fa5d09b866a6
Daniel Johnson <dan@balancerockmedia.com>
2014-07-27 10:12:31 -0400

    edits

--- markets-ui-ember/app/models/market.js
+++ markets-ui-ember/app/models/market.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    currency_id: DS.attr('number'),
-    location_id: DS.attr('number'),
+    currency: DS.belongsTo('currency'),
+    location: DS.belongsTo('location'),
     start_date: DS.attr('date'),
     end_date: DS.attr('date')
 });
