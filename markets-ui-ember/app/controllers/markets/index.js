faa952b5840b55956ecc68cc80c400d826e5dfdc
Daniel Johnson <dan@balancerockmedia.com>
2014-07-28 16:45:29 -0400

    edits

--- markets-ui-ember/app/controllers/markets/index.js
+++ markets-ui-ember/app/controllers/markets/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  itemController: 'market',
+  
   totalMarkets: function() {
     return 'Total Markets: ' + this.get('model').get('length');
   }.property('@each')
