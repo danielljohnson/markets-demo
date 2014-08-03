31f1d9f848ab702b6778754ee53d0b033cc62bc6
Daniel Johnson <dan@balancerockmedia.com>
2014-08-03 11:05:55 -0400

    edits

--- markets-ui-ember/app/views/market-modal.js
+++ markets-ui-ember/app/views/market-modal.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   didInsertElement: function() {
+    var viewContext = this;
+    
     this.$('.modal').modal('show');
+    
+    this.$('.modal').on('hidden.bs.modal', function(e) {
+      viewContext.get('controller').send('closeModal');
+    });
   }
 });
